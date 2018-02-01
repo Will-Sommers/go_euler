9cf58bfa3ced4fee5c2b731dcf7ca3e7c1ed5974
Will Sommers <will.sommers@gmail.com>
2018-01-31 23:56:30 -0500

    finish up problem 4

--- problem_4.go
+++ problem_4.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 )
 
-func is_palindrome(number int64) bool{
-	s := strconv.FormatInt(number, 10)
+func is_palindrome(number int) bool{
+	s := strconv.FormatInt(int64(number), 10)
 	chars := strings.Split(s, "")
 	string_length := len(chars)
 	for i := 0; i < string_length; i++ {
@@ -22,14 +22,27 @@ func is_palindrome(number int64) bool{
 }
 
 func main() {
-	fmt.Printf("%t", is_palindrome(12131))
 	nums := []int{}
-	for i := 100; i <= 1000; i++ {
-		nums = append(nums, i)
-	}
 	nums_2 := []int{}
-	copy(nums_2, nums)
-
-	for num := range nums {
+	for i := 100; i < 1000; i++ {
+		nums = append(nums, i)
+		nums_2 = append(nums_2, i)
 	}
+
+
+
+	answer := 0
+	for i := range nums {
+		for j := range nums_2 {
+			test_value := nums[i] * nums_2[j]
+			//fmt.Printf("%d", test_value)
+			if is_palindrome(test_value) {
+				if test_value > answer {
+					answer = test_value
+				}
+
+			}
+		}
+	}
+	fmt.Printf("%d", answer)
 }
